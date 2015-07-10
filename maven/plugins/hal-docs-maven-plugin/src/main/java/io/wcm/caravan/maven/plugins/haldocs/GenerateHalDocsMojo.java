6ffeaa14bae9513e11432fa0af8aae639da21375
sseifert <sseifert@pro-vision.de>
2015-07-10 17:28:30 +0200

    define annotations for ServiceInfo class

--- maven/plugins/hal-docs-maven-plugin/src/main/java/io/wcm/caravan/maven/plugins/haldocs/GenerateHalDocsMojo.java
+++ maven/plugins/hal-docs-maven-plugin/src/main/java/io/wcm/caravan/maven/plugins/haldocs/GenerateHalDocsMojo.java
@@ -19,8 +19,14 @@
  */
 package io.wcm.caravan.maven.plugins.haldocs;
 
+import io.wcm.caravan.commons.haldocs.annotations.LinkRelationDoc;
+import io.wcm.caravan.commons.haldocs.annotations.ServiceDoc;
 import io.wcm.caravan.maven.plugins.haldocs.generator.ServiceDocGenerator;
+import io.wcm.caravan.maven.plugins.haldocs.model.LinkRelation;
 import io.wcm.caravan.maven.plugins.haldocs.model.Service;
+
+import java.io.File;
+import java.util.stream.Collectors;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -28,11 +34,23 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import com.thoughtworks.qdox.JavaProjectBuilder;
+import com.thoughtworks.qdox.model.JavaAnnotatedElement;
+import com.thoughtworks.qdox.model.JavaAnnotation;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaField;
+
 /**
  * Generates HAL documentation for service.
  */
-@Mojo(name = "generate-hal-docs", defaultPhase = LifecyclePhase.GENERATE_RESOURCES, requiresProject = true, threadSafe = true)
+@Mojo(name = "generate-hal-docs", defaultPhase = LifecyclePhase.PROCESS_CLASSES, requiresProject = true, threadSafe = true)
 public class GenerateHalDocsMojo extends AbstractBaseMojo {
+
+  /**
+   * Paths containing the java source files.
+   */
+  @Parameter(defaultValue = "${basedir}/src/main/java")
+  private String source;
 
   /**
    * Service ID
@@ -72,10 +90,59 @@ public class GenerateHalDocsMojo extends AbstractBaseMojo {
   private Service getServiceInfos() {
     Service service = new Service();
 
+    // get some service properties from pom
     service.setServiceId(serviceId);
     service.setName(project.getName());
 
+    // find @ServiceDoc annotated class in source folder
+    JavaProjectBuilder builder = new JavaProjectBuilder();
+    builder.addSourceTree(new File(source));
+    JavaClass serviceInfo = builder.getSources().stream()
+        .flatMap(javaSource -> javaSource.getClasses().stream())
+        .filter(javaClass -> hasAnnotation(javaClass, ServiceDoc.class))
+        .findFirst().orElse(null);
+
+    // populate further service information from @ServiceDoc class and @LinkRelationDoc fields
+    if (serviceInfo != null) {
+      service.setDescriptionMarkup(serviceInfo.getComment());
+      service.setLinkRelations(serviceInfo.getFields().stream()
+          .filter(field -> hasAnnotation(field, LinkRelationDoc.class))
+          .map(this::toLinkRelation)
+          .collect(Collectors.toList()));
+    }
+
     return service;
+  }
+
+  private LinkRelation toLinkRelation(JavaField field) {
+    LinkRelation rel = new LinkRelation();
+    rel.setDescriptionMarkup(field.getComment());
+    rel.setRel(field.getInitializationExpression());
+    rel.setJsonSchemaRef(getJsonSchemaRef(field));
+    return rel;
+  }
+
+  private String getJsonSchemaRef(JavaField field) {
+    return getAnnotationValue(field, LinkRelationDoc.class, "jsonSchema", String.class);
+  }
+
+  private boolean hasAnnotation(JavaAnnotatedElement clazz, Class<?> annotationClazz) {
+    return clazz.getAnnotations().stream()
+        .filter(item -> item.getType().isA(annotationClazz.getName()))
+        .count() > 0;
+  }
+
+  @SuppressWarnings("unchecked")
+  private <T> T getAnnotationValue(JavaAnnotatedElement clazz, Class<?> annotationClazz, String property, Class<T> propertyType) {
+    JavaAnnotation annotation = clazz.getAnnotations().stream()
+        .filter(item -> item.getType().isA(annotationClazz.getName()))
+        .findFirst().orElse(null);
+    if (annotation != null) {
+      return (T)annotation.getNamedParameter(property);
+    }
+    else {
+      return null;
+    }
   }
 
   @Override
